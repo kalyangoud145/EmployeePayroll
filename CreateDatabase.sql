bbd16b8e947a5855dd5c86ece480a612a4f2e66a
kalyangoud145 <kalyangoud145@gmail.com>
2020-11-20 14:03:37 +0530

    [Kalyan] added employee payroll table with columns

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -4,3 +4,11 @@ Create Database Payroll_Service
 EXEC sp_databases;  
 --switch to  payroll_service database
 use payroll_service;
+--CREATE a table Employee_Payroll with id,name,salary and start date columns
+CREATE TABLE Employee_Payroll
+(
+    Id INT PRIMARY KEY NOT NULL IDENTITY(1,1),
+    Name VARCHAR(30) NOT NULL,
+    Salary money NOT NULL,
+    Start_Date DATE NOT NULL
+);
