5579cae08fc6cf31305f59994d0ae6391d0bc534
kalyangoud145 <kalyangoud145@gmail.com>
2020-11-20 19:19:32 +0530

    [Kalyan] added terisa to two different departments

---
+++ AddedTerisaToSalesAndMarketing.sql
@@ -0,0 +1,13 @@
+-- Adding Terisa to both sales and marketing department
+update Employee_Payroll
+set Department = 'Sales' 
+where Name = 'Terisa';
+
+insert into Employee_Payroll
+(
+    Name,Salary,Start_Date,Gender,Address,PhoneNumber,Department,basic_pay,Net_Pay
+)
+VALUES
+(
+    'Terisa',60000.00,'2018-01-03','F','HYD','9010463323','Marketing',25000.00,50000.00
+);
